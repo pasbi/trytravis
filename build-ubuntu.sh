186137dfcc9a0c6965f81d9a0815c7122beeecd6
Pascal Bies <pascal.bies@k-lens.de>
2019-11-03 13:12:21 +0100

    uu

--- build-ubuntu.sh
+++ build-ubuntu.sh
@@ -26,22 +26,27 @@ case "$dist" in
   C_COMPILER=gcc-8
   QT_PREFIX="/opt/qt512"
   QT_QM_PATH="/usr/share/qt5/translations/"
+
+  # Download cmake binaries
+  wget https://github.com/Kitware/CMake/releases/download/v3.14.5/cmake-3.14.5-Linux-x86_64.tar.gz
+  tar xf cmake-3.14.5-Linux-x86_64.tar.gz
+  cmake=$(pwd)/cmake-3.14.5-Linux-x86_64/bin/cmake
   ;;
- "disco" | "eoan")
-  sudo apt install -y g++ gcc
+"disco" | "eoan")
+  sudo apt install -y g++ gcc pybind11-dev cmake
   sudo apt install -y qtbase5-dev qtchooser qt5-qmake \
                       qtbase5-dev-tools qt5-default \
                       libqt5svg5-dev qttools5-dev \
-                      qttools5-dev pybind11-dev
+                      qttools5-dev
   CXX_COMPILER=g++
   C_COMPILER=gcc
+  cmake=cmake
+  QT_PREFIX=""
+  QT_QM_PATH="/usr/share/qt5/translations/"
   ;;
 esac
 sudo apt install -y libpoppler-qt5-dev libkf5itemmodels-dev
 
-wget https://github.com/Kitware/CMake/releases/download/v3.14.5/cmake-3.14.5-Linux-x86_64.tar.gz
-tar xf cmake-3.14.5-Linux-x86_64.tar.gz
-cmake_3_14=$(pwd)/cmake-3.14.5-Linux-x86_64/bin/cmake
 
 if [ -d build ]; then
   rm -r build
@@ -49,13 +54,13 @@ fi
 mkdir build
 
 cd build
-$cmake_3_14 -GNinja \
-            -DCMAKE_BUILD_TYPE=Release \
-            -DCMAKE_CXX_COMPILER="$CXX_COMPILER" \
-            -DCMAKE_C_COMPILER="$C_COMPILER" \
-            -DQT_QM_PATH="$QT_QM_PATH" \
-            -DCMAKE_PREFIX_PATH="$QT_PREFIX" \
-            -DCMAKE_INSTALL_PREFIX=/usr \
-            ..
+$cmake -GNinja \
+       -DCMAKE_BUILD_TYPE=Release \
+       -DCMAKE_CXX_COMPILER="$CXX_COMPILER" \
+       -DCMAKE_C_COMPILER="$C_COMPILER" \
+       -DQT_QM_PATH="$QT_QM_PATH" \
+       -DCMAKE_PREFIX_PATH="$QT_PREFIX" \
+       -DCMAKE_INSTALL_PREFIX=/usr \
+       ..
 
 ninja
