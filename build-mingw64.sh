e69dd0b01280ae0680a458f0246e5efa1f4c71c5
Pascal Bies <pascal.bies@k-lens.de>
2020-10-11 17:26:01 +0200

    x

--- build-mingw64.sh
+++ build-mingw64.sh
@@ -1,13 +1,10 @@
 echo "Hello from the try travis build script for the MinGW64 platform"
 
-pacman -sS qt5
-pacman -sS poppler
-pacman -sS python
-
-pacman -S mingw-w64-x86_64-ninja \
-          mingw-w64-x86_64-qt5 \
-          mingw-w64-x86_64-poppler \
-          mingw-w64-x86_64-python
+pacman --noconfirm -S \
+  mingw-w64-x86_64-ninja \
+  mingw-w64-x86_64-qt5 \
+  mingw-w64-x86_64-poppler \
+  mingw-w64-x86_64-python
 
 echo "\n\nQt content begin\n======="
 pacman -Ql mingw-w64-qt5
