1f41fc405bac545a2ebcbc6d233094d4ac01eae7
Pascal Bies <pascal.bies@k-lens.de>
2020-10-11 14:36:08 +0200

    x

--- build-mingw64.sh
+++ build-mingw64.sh
@@ -1,9 +1,19 @@
 echo "Hello from the try travis build script for the MinGW64 platform"
 
-make --version
-gcc --version
-g++ --version
-cmake --version
-pacman --version
-ninja --version
+pacman -S mingw-w64-x86_64-ninja \
+          mingw-w64-qt5 \
+          mingw-w64-poppler \
+          mingw-w64-python
+
+echo "\n\nQt content begin\n======="
+pacman -Ql mingw-w64-qt5
+echo "\n\nQt content end\n======="
+
+build_dir="build"
+cmake -GNinja \
+  -B "$build_dir" \
+  -S . \
+  -DQT_QM_PATH="/mingw64/usr/share/qt/translations/"
+
+cmake --build "$build_dir"
 
