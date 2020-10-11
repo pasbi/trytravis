cc154d481ac3e2deddb07003a698460254d05a23
Pascal Bies <pascal.bies@k-lens.de>
2020-10-11 18:50:47 +0200

    x

--- build-mingw64.sh
+++ build-mingw64.sh
@@ -1,5 +1,19 @@
 echo "Hello from the try travis build script for the MinGW64 platform"
 
+pacman --noconfirm -S \
+    mingw-w64-x86_64-ninja \
+    mingw-w64-x86_64-qt5 \
+    mingw-w64-x86_64-poppler \
+    mingw-w64-x86_64-python \
+    mingw-w64-x86_64-pybind11 \
+    mingw-w64-x86_64-kitemmodel-qt5 \
+    mingw-w64-x86_64-openssl \
+    mingw-w64-x86_64-libffi \
+    mingw-w64-x86_64-zlib \
+    mingw-w64-x86_64-python-pytest
+
+pacman -Q
+
 build_dir="build"
 cmake -GNinja \
   -B "$build_dir" \
